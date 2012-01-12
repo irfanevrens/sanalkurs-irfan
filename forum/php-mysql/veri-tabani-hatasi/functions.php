6682e9ec135b51000166301f68377dc4724231f9
irfanevrens@gmail.com <irfanevrens@gmail.com@6485184e-8954-01bb-c24a-e59fb2e04fc9>
2012-01-12 12:04:03 +0000

    tab sorunu giderildi

--- forum/php-mysql/veri-tabani-hatasi/functions.php
+++ forum/php-mysql/veri-tabani-hatasi/functions.php
@@ -14,20 +14,20 @@ function menu_olustur($sayfa = '') {
 	echo '<li><a' . $aktifMenu . ' href="icerik.php">Anasayfa</li>' . "\n";
    
 	// diğer sayfaların menüde gösterilmesi için veritabanına sorgu gönderiyoruz
-    $sorgu = mysql_query('SELECT * FROM sayfalar ORDER BY sira ASC');
-    
-    // sorgu sonucu üzerinde dönmeye başlıyoruz
-    while ($bilgi = mysql_fetch_array($sorgu)) {
+	$sorgu = mysql_query('SELECT * FROM sayfalar ORDER BY sira ASC');
+
+	// sorgu sonucu üzerinde dönmeye başlıyoruz
+	while ($bilgi = mysql_fetch_array($sorgu)) {
       
-    	// aktif sayfamızı tesbit etmek amacı ile kontrol yapıyoruz
-    	$aktifMenu = '';
-    	if ($sayfa == $bilgi['id'])
-    		$aktifMenu = ' class="aktifMenu"';
+		// aktif sayfamızı tesbit etmek amacı ile kontrol yapıyoruz
+		$aktifMenu = '';
+		if ($sayfa == $bilgi['id'])
+			$aktifMenu = ' class="aktifMenu"';
     	
-    	// menü elemanını ekrana yazdırıyoruz
-    	echo '<li><a' . $aktifMenu . ' href="' . $_SERVER['PHP_SELF'] . '?sayfa=' . $bilgi['id'] . '">' . $bilgi['isim'] . '</li>' . "\n";
-    }
+		// menü elemanını ekrana yazdırıyoruz
+		echo '<li><a' . $aktifMenu . ' href="' . $_SERVER['PHP_SELF'] . '?sayfa=' . $bilgi['id'] . '">' . $bilgi['isim'] . '</li>' . "\n";
+	}
     
-    // menü listemizi tamamlıyoruz
-    echo '</ul>' . "\n";
+	// menü listemizi tamamlıyoruz
+	echo '</ul>' . "\n";
 }
